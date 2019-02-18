fde35fed8d2ed75dddc4d4cc1835ab95d1669b2e
Adam Luckey <adamluckey@opsstate.com>
2019-02-18 10:45:18 -0800

    Fixed typos in filenames

--- lib/opsstate_view_tool.rb
+++ lib/opsstate_view_tool.rb
@@ -2,8 +2,6 @@ require "opsstate_view_tool/version"
 require "opsstate_view_tool/renderer"
 
 module OpsstateViewTool
-  class Renderer
-    def self.copyright name, msg
-        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
+  class Error < StandardError; end
+  # Your code goes here...
 end
